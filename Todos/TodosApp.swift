87e7071f898ee32a22c0bfef11321ee8ade3577d
Peter Alt <peter@peteralt.com>
2023-02-21 14:12:27 -0700

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Peter Alt on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
